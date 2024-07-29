5ee9bc6c3f023d58de66e4a5fe3cc07dc018163d
Alberto Riffaud <alriffaud@gmail.com>
2024-07-29 11:30:03 -0300

    Creation of 6-howdy_partner.py file.

---
+++ math/linear_algebra/6-howdy_partner.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""This file defines the cat_arrays function"""
+
+
+def cat_arrays(arr1, arr2):
+    """
+    This function concatenates two arrays.
+    Args:
+        arr1 (list): this is the first array to concatenate.
+        arr2 (list): this is the second array to concatenate.
+    """
+    return arr1 + arr2
