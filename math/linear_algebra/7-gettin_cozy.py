cca4e361d292aca8c4b686c2e699701e4c27af34
Alberto Riffaud <alriffaud@gmail.com>
2024-07-29 11:47:24 -0300

    Creation of 7-gettin_cozy.py file.

---
+++ math/linear_algebra/7-gettin_cozy.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""This file defines the cat_matrices2D function"""
+
+
+def cat_matrices2D(mat1, mat2, axis=0):
+    """_summary_
+
+    Args:
+        mat1 (list): this is the first matrix to concatenate.
+        mat2 (list): this is the second matrix to concatenate.
+        axis (int, optional): this is the axis to concatenate. Defaults to 0.
+
+    Returns:
+        This function returns a new matrix. If the two matrices cannot be
+        concatenated, return None.
+    """
+    if axis == 0:
+        return mat1 + mat2
+    elif axis == 1:
+        if len(mat1) != len(mat2):
+            return None
+        else:
+            return [mat1[i] + mat2[i] for i in range(len(mat1))]
+    else:
+        return None
