f503678c03215d5e6641781bc69b3c2aaa99a0dc
alriffaud <alriffaud@gmail.com>
2024-07-30 16:23:19 +0000

    Creation of 11-the_western_exchange.py file.

---
+++ math/linear_algebra/11-the_western_exchange.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""This file defines the np_transpose function"""
+
+
+def np_transpose(matrix):
+    """
+    This function calculates the transpose of a numpy.ndarray.
+    Args:
+        matrix (list): this is the matrix to calculate its transpose.
+    Returns:
+        This function returns the transpose of the matrix.
+    """
+    return matrix.T
