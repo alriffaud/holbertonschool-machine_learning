e476e5cefe5366f875e03588f42e65b09705dc76
alriffaud <alriffaud@gmail.com>
2024-07-30 15:58:51 +0000

    Modification of 10-ill_use_my_scale.py file.

--- math/linear_algebra/10-ill_use_my_scale.py
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -1,15 +1,14 @@
 #!/usr/bin/env python3
 """This file defines the np_shape function"""
-import numpy as np
 
 
 def np_shape(matrix):
     """
     This function calculates the shape of a numpy.ndarray.
     Args:
-        matrix: this is the matrix to calculate its shape.
+        matrix (list): this is the matrix to calculate its shape.
     Returns:
         This function returns a tuple of integers representing the shape
         of the matrix.
     """
-    return np.shape(matrix)
+    return matrix.shape
