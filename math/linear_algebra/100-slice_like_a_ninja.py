9cc49aff6b3b269f42ab62743a6bb9ecb9d4c49f
Alberto Riffaud <alriffaud@gmail.com>
2024-07-31 09:08:36 -0300

    Creation of 100-slice_like_a_ninja.py file.

---
+++ math/linear_algebra/100-slice_like_a_ninja.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""This file defines the np_slice function"""
+import numpy as np
+
+
+def np_slice(matrix, axes={}):
+    """
+    This function slices a matrix along specific axes.
+    Args:
+        matrix (numpy.ndarray): this is the matrix to slice.
+        axes: a dictionary where the key is an axis to slice along and the
+        value is a tuple representing the slice to make along that axis.
+    Returns:
+        This function returns a matrix representing a matrix along specific
+        axes.
+    """
+    slices = [slice(None)] * matrix.ndim
+    for axis, slice_tuple in axes.items():
+        slices[axis] = slice(*slice_tuple)
+    return matrix[tuple(slices)]
