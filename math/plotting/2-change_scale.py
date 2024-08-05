7cb50d5f7689db0f712e104926e1d50cf1320175
Alberto Riffaud <alriffaud@gmail.com>
2024-08-05 09:53:14 -0300

    Creation of 2-change_scale.py file.

---
+++ math/plotting/2-change_scale.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""This file defines the change_scale function."""
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def change_scale():
+    """
+    This function plots x ↦ y as a line graph.
+    """
+    x = np.arange(0, 28651, 5730)
+    r = np.log(0.5)
+    t = 5730
+    y = np.exp((r / t) * x)
+    plt.figure(figsize=(6.4, 4.8))
+    plt.plot(x, y)
+    plt.title('Exponential Decay of C-14')
+    plt.xlabel('Time (years)')
+    plt.ylabel('Fraction Remaining')
+    plt.axis([0, 28650, None, None])
+    plt.yscale('log')
+    plt.show()
