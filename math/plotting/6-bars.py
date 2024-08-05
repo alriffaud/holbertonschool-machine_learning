4cb9e5886090305c9beb001b4829f1e9123d6874
Alberto Riffaud <alriffaud@gmail.com>
2024-08-05 14:05:57 -0300

    Modification of 6-bars.py file.

---
+++ math/plotting/6-bars.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+"""This file defines the bars function."""
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def bars():
+    """
+    This function plots a stacked bar graph.
+    """
+    np.random.seed(5)
+    fruit = np.random.randint(0, 20, (4, 3))
+    plt.figure(figsize=(6.4, 4.8))
+
+    people = ['Farrah', 'Fred', 'Felicia']
+    apples = fruit[0]
+    bananas = fruit[1]
+    oranges = fruit[2]
+    peaches = fruit[3]
+
+    r = np.arange(len(people))
+    plt.bar(r, apples, color='r', edgecolor='black', width=0.5, label='apples')
+    plt.bar(r, bananas, bottom=apples, color='y', edgecolor='black', width=0.5,
+            label='bananas')
+    plt.bar(r, oranges, bottom=np.array(apples) + np.array(bananas),
+            color='#ff8000', edgecolor='black', width=0.5, label='oranges')
+    plt.bar(r, peaches, bottom=np.array(apples) + np.array(bananas) +
+            np.array(oranges), color='#ffe5b4', edgecolor='black', width=0.5,
+            label='peaches')
+
+    plt.legend()
+    plt.yticks(np.arange(0, 90, 10))
+    plt.axis([None, None, 0, 80])
+
+    # Adding labels
+    plt.ylabel('Quantity of Fruit')
+    plt.title('Number of Fruit per Person')
+    plt.xticks(r, people)
+
+    # Display the plot
+    plt.tight_layout()
+    plt.show()
