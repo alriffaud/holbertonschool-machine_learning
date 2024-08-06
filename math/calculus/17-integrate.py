2b1df2d2dab165ad3a444bc5d176355e6e015190
alriffaud <alriffaud@gmail.com>
2024-08-06 15:14:49 +0000

    Creation of 17-integrate.py file.

---
+++ math/calculus/17-integrate.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""This file defines the poly_integral function."""
+
+
+def poly_integral(poly, C=0):
+    """
+    This function calculates the derivative of a polynomial.
+    """
+    if not isinstance(poly, list) or len(poly) == 0 or not isinstance(C, int):
+        return None
+    n = len(poly)
+    poly_int = [C]
+    for k in range(n):
+        new_coef = poly[k] / (k + 1)
+        if new_coef.is_integer():
+            new_coef = int(new_coef)
+        poly_int.append(new_coef)
+    return poly_int
