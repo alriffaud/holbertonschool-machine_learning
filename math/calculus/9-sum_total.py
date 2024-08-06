4d42e5cc727014d401767ad4f1b8728632d6a453
alriffaud <alriffaud@gmail.com>
2024-08-06 13:25:19 +0000

    Creation of 9-sum_total.py file.

---
+++ math/calculus/9-sum_total.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""This file defines the summation_i_squared function."""
+
+
+def summation_i_squared(n):
+    """
+    This function that calculates the sum of i^2 from 1 to n.
+    """
+    if not isinstance(n, int):
+        return None
+    result = n * (n + 1) * (2 * n + 1) // 6
+    return result
