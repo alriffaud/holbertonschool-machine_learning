ac66c82797abcac9d08bca609f8ba6e1a83b36d5
alriffaud <alriffaud@gmail.com>
2024-08-06 13:27:51 +0000

    Modification of 9-sum_total.py file.

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -6,7 +6,7 @@ def summation_i_squared(n):
     """
     This function that calculates the sum of i^2 from 1 to n.
     """
-    if not isinstance(n, int):
+    if type(n) != int:
         return None
     result = n * (n + 1) * (2 * n + 1) // 6
     return result
