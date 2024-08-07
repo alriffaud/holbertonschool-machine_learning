c4907855091b5b9fc32447809dbd9a8fe4ae0aa6
Alberto Riffaud <alriffaud@gmail.com>
2024-08-07 10:20:20 -0300

    Modification of 100-gradient.py file.

---
+++ math/plotting/100-gradient.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""This file defines the gradient function."""
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def gradient():
+    """
+    This function creates a scatter plot of sampled elevations on a mountain.
+    """
+    np.random.seed(5)
+
+    x = np.random.randn(2000) * 10
+    y = np.random.randn(2000) * 10
+    z = np.random.rand(2000) + 40 - np.sqrt(np.square(x) + np.square(y))
+    plt.figure(figsize=(6.4, 4.8))
+
+    scatter = plt.scatter(x, y, c=z)
+    cbar = plt.colorbar(scatter)
+    cbar.set_label('elevation (m)')
+    plt.title('Mountain Elevation')
+    plt.xlabel('x coordinate (m)')
+    plt.ylabel('y coordinate (m)')
+    plt.tight_layout()
+    plt.show()
