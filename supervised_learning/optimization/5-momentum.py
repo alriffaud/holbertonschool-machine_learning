c56bde3fc78e172cb7ed845b77c5a28284f96d77
Alberto Riffaud <alriffaud@gmail.com>
2024-09-12 12:32:33 -0300

    Modification of 5-momentum.py file.

--- supervised_learning/optimization/5-momentum.py
+++ supervised_learning/optimization/5-momentum.py
@@ -17,6 +17,8 @@ def update_variables_momentum(alpha, beta1, var, grad, v):
         v (numpy.ndarray): is the previous first moment of var.
     Returns: the updated variable and the new moment, respectively.
     """
+    # Compute the first moment of var using the gradient of var
     v = beta1 * v + (1 - beta1) * grad
+    # Update the variable using the gradient descent with momentum algorithm
     var = var - alpha * v
     return var, v
