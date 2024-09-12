44458e0c85fe437d40e484a370a0760621554dd5
Alberto Riffaud <alriffaud@gmail.com>
2024-09-12 10:46:33 -0300

    Creation of 4-moving_average.py file.

---
+++ supervised_learning/optimization/4-moving_average.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+""" This module defines the moving_average function. """
+import numpy as np
+shuffle_data = __import__('2-shuffle_data').shuffle_data
+
+
+def moving_average(data, beta):
+    """
+    This function calculates the weighted moving average of a data set using
+    bias correction.
+    Args:
+        data (numpy.ndarray):  is the list of data to calculate the moving
+        average of.
+        beta (float): is the weight used for the moving average.
+    Returns: a list containing the moving averages of data.
+    """
+    v = 0
+    moving_averages = []
+    for i in range(len(data)):
+        v = beta * v + (1 - beta) * data[i]
+        moving_averages.append(v / (1 - beta ** (i + 1)))
+    return moving_averages
