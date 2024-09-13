a2aa2a477e1911eea061ca7b37a8685189b640c0
Alberto Riffaud <alriffaud@gmail.com>
2024-09-13 10:21:10 -0300

    Modification of 9-Adam.py file.

--- supervised_learning/optimization/9-Adam.py
+++ supervised_learning/optimization/9-Adam.py
@@ -26,9 +26,10 @@ def update_variables_Adam(alpha, beta1, beta2, epsilon, var, grad, v, s, t):
     v = beta1 * v + (1 - beta1) * grad
     # Compute the second moment of var using the gradient of var
     s = beta2 * s + (1 - beta2) * np.square(grad)
-    # Corrections of first and second moment
-    v = v / (1 - beta1 ** t)
-    s = s / (1 - beta2 ** t)
-    # Update the variable using the Adam algorithm
-    var = var - (alpha / (np.sqrt(s) + epsilon)) * v
+    # Compute the bias-corrected first moment
+    v_corrected = v / (1 - beta1 ** t)
+    # Compute the bias-corrected second moment
+    s_corrected = s / (1 - beta2 ** t)
+    # Update the variable using the Adam optimization algorithm
+    var = var - (alpha / (np.sqrt(s_corrected) + epsilon)) * v_corrected
     return var, v, s
