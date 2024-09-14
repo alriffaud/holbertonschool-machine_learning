07e419d62f4f7a29e8fc420d6816ed561b13fa0f
Alberto Riffaud <alriffaud@gmail.com>
2024-09-14 09:40:17 -0300

    Creation of 13-batch_norm.py file.

---
+++ supervised_learning/optimization/13-batch_norm.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+""" This module defines the batch_norm function. """
+import numpy as np
+
+
+def batch_norm(Z, gamma, beta, epsilon):
+    """
+    This function normalizes an unactivated output of a neural network using
+    batch normalization.
+    Args:
+        Z (numpy.ndarray): numpy array of shape (m, n) that should be
+        normalized. m is the number of data points. n is the number of features
+        in Z
+        gamma (numpy.ndarray): numpy array of shape (1, n) containing the
+        scales used for batch normalization.
+        beta (numpy.ndarray): numpy array of shape (1, n) containing the
+        offsets used for batch normalization.
+        epsilon (float): is a small number used to avoid division by zero.
+    Returns: the normalized Z matrix
+    """
+    m = Z.mean(axis=0)
+    s = Z.std(axis=0)
+    Z_norm = (Z - m) / (s + epsilon)
+    return gamma * Z_norm + beta
