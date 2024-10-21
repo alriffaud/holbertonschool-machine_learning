bdd8df023805e2db07ecb9ecff8f4cc89b399f49
Alberto Riffaud <alriffaud@gmail.com>
2024-10-21 09:36:45 -0300

    Creation of 0-neural_style.py file.

---
+++ supervised_learning/neural_style_transfer/0-neural_style.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+""" This module defines the class NST that performs Neural Style Transfer """
+import numpy as np
+import tensorflow as tf
+
+
+class NST:
+    """ This class performs tasks for Neural Style Transfer """
+    style_layers = ['block1_conv1',
+                    'block2_conv1',
+                    'block3_conv1',
+                    'block4_conv1',
+                    'block5_conv1']
+    content_layer = 'block5_conv2'
+
+    def __init__(self, style_image, content_image, alpha=1e4, beta=1):
+        """
+        Class constructor for NST class that initializes the following public
+        instance attributes:
+        - style_image (numpy.ndarray): the image used as a style reference.
+        - content_image (numpy.ndarray): the image used as a content reference.
+        - alpha (float): the weight for content cost.
+        - beta (float): the weight for style cost.
+        """
+        if (not isinstance(style_image, np.ndarray)
+                or len(style_image.shape) != 3):
+            raise TypeError("style_image must be a numpy.ndarray with shape \
+                (h, w, 3)")
+        if (not isinstance(content_image, np.ndarray)
+                or len(content_image.shape) != 3):
+            raise TypeError("content_image must be a numpy.ndarray with shape \
+                (h, w, 3)")
+        if not isinstance(alpha, (int, float)) or alpha < 0:
+            raise TypeError("alpha must be a non-negative number")
+        if not isinstance(beta, (int, float)) or beta < 0:
+            raise TypeError("beta must be a non-negative number")
+
+        self.style_image = self.scale_image(style_image)
+        self.content_image = self.scale_image(content_image)
+        self.alpha = alpha
+        self.beta = beta
+
+    @staticmethod
+    def scale_image(image):
+        """
+        This method rescales the image such that its pixels values are between
+        0 and 1 and its largest side is 512 pixels.
+        Args:
+            image (numpy.ndarray): the image to be rescaled.
+        """
+        if not isinstance(image, np.ndarray) or len(image.shape) != 3:
+            raise TypeError("image must be a numpy.ndarray with shape \
+                (h, w, 3)")
+        h, w, _ = image.shape
+        if h > w:
+            h_new = 512
+            w_new = int(w * h_new / h)
+        else:
+            w_new = 512
+            h_new = int(h * w_new / w)
+        image = tf.image.resize(image, (h_new, w_new))
+        image = tf.clip_by_value(image / 255.0, 0, 1)
+        return image[tf.newaxis, ...]
