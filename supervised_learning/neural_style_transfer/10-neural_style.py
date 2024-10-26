fbd9afa0f9c729edfe7fadc4acb5ad2806513376
Alberto Riffaud <alriffaud@gmail.com>
2024-10-26 17:10:30 -0300

    Modification of 10-neural_style.py file.

--- supervised_learning/neural_style_transfer/10-neural_style.py
+++ supervised_learning/neural_style_transfer/10-neural_style.py
@@ -306,10 +306,12 @@ class NST:
             raise TypeError("iterations must be an integer")
         if iterations <= 0:
             raise ValueError("iterations must be positive")
+        if iterations == 1:
+            step = 1
         if step is not None:
             if not isinstance(step, int):
                 raise TypeError("step must be an integer")
-            if step <= 0 or step >= iterations:
+            if step <= 0 or step > iterations:
                 raise ValueError(
                     "step must be positive and less than iterations")
         if not isinstance(lr, (int, float)):
@@ -336,19 +338,21 @@ class NST:
 
         # Special case for when iterations == 1
         if iterations == 1:
-            # Calculate gradients and costs for one iteration
-            grads, J_total, J_content, J_style, J_var = self.compute_grads(
-                generated_image)
+            with tf.GradientTape() as tape:
+                grads, J_total, J_content, J_style, J_var = self.compute_grads(
+                    generated_image)
+
+            # Applying gradients to the generated image
             optimizer.apply_gradients([(grads, generated_image)])
             generated_image.assign(tf.clip_by_value(generated_image, 0, 1))
 
-            # Store the best image
-            best_cost = J_total
-            best_image = generated_image[0]
-            print(f"Cost at iteration {1}: {J_total.numpy()}, \
+            print(f"Cost at iteration {0}: {J_total.numpy()}, \
 content {J_content.numpy()}, style {J_style.numpy()}, var {J_var.numpy()}")
 
-            return best_image.numpy(), best_cost.numpy()
+            prev_image = generated_image
+            # Removes the extra dimension from the image
+            best_image = prev_image[0]
+            return best_image.numpy(), J_total.numpy()
 
         for i in range(iterations + 1):
             with tf.GradientTape() as tape:
