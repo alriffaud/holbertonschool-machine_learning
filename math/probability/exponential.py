e0c20887d73c68090bac6fbe195f3a9420de8315
Alberto Riffaud <alriffaud@gmail.com>
2024-11-06 10:50:41 -0300

    Modification of exponential.py file.

--- math/probability/exponential.py
+++ math/probability/exponential.py
@@ -21,3 +21,16 @@ class Exponential:
             if len(data) < 2:
                 raise ValueError("data must contain multiple values")
             self.lambtha = len(data) / sum(data)
+    
+    def pdf(self, x):
+        """
+        This method calculates the value of the PDF for a given time period.
+        Args:
+            - x: The time period.
+        Returns:
+            The PDF value for x.
+        """
+        if x < 0:
+            return 0
+        e = 2.7182818285
+        return self.lambtha * (e ** (-self.lambtha * x))
