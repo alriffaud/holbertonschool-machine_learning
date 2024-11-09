cbd779cde42575c4a4f1c4e12de73b53ca66a3a9
Alberto Riffaud <alriffaud@gmail.com>
2024-11-09 18:45:24 -0300

    Modification of 2-build_decision_tree.py file documentation.

--- supervised_learning/decision_tree/2-build_decision_tree.py
+++ supervised_learning/decision_tree/2-build_decision_tree.py
@@ -8,7 +8,16 @@ class Node:
     decision node or a leaf. """
     def __init__(self, feature=None, threshold=None, left_child=None,
                  right_child=None, is_root=False, depth=0):
-        """ This is the __init__ method. """
+        """
+        This is the __init__ method.
+        Args:
+            feature (int): the index of the feature to split on.
+            threshold (float): the threshold to split the feature.
+            left_child (Node): the left child of the node.
+            right_child (Node): the right child of the node.
+            is_root (bool): a boolean indicating if the node is the root.
+            depth (int): the depth of the node in the tree.
+        """
         self.feature = feature
         self.threshold = threshold
         self.left_child = left_child
@@ -35,8 +44,13 @@ class Node:
         return max(left_depth, right_depth)
 
     def count_nodes_below(self, only_leaves=False):
-        """ This method returns the number of nodes or leaves in the subtree
-        from the current node. """
+        """
+        This method returns the number of nodes or leaves in the subtree
+        from the current node.
+        Args:
+            only_leaves (bool): a boolean indicating if we should count only
+                the leaves.
+        """
         if self.is_leaf:
             return 1
         if only_leaves:
@@ -61,8 +75,12 @@ class Node:
             return left_leaves + right_leaves + 1
 
     def left_child_add_prefix(self, text):
-        """ This method adds visual prefixes to show the tree hierarchy when
-        it is printed. """
+        """
+        This method adds visual prefixes to show the tree hierarchy when
+        it is printed.
+        Args:
+            text (str): the text to add the prefix to.
+        """
         lines = text.split("\n")
         new_text = "    +--" + lines[0] + "\n"
         for x in lines[1:]:
@@ -70,8 +88,12 @@ class Node:
         return (new_text)
 
     def right_child_add_prefix(self, text):
-        """ This method adds visual prefixes to show the tree hierarchy when
-        it is printed. """
+        """
+        This method adds visual prefixes to show the tree hierarchy when
+        it is printed.
+        Args:
+            text (str): the text to add the prefix to.
+        """
         lines = text.split("\n")
         new_text = "    +--" + lines[0] + "\n"
         for x in lines[1:]:
@@ -96,7 +118,12 @@ class Leaf(Node):
     """ This class inherits from Node and represents a leaf in the decision
     tree. """
     def __init__(self, value, depth=None):
-        """ This is the __init__ method. """
+        """
+        This is the __init__ method.
+        Args:
+            value (int): the value of the leaf.
+            depth (int): the depth of the leaf in the tree.
+        """
         super().__init__()
         self.value = value
         self.is_leaf = True
@@ -108,8 +135,13 @@ class Leaf(Node):
         return self.depth
 
     def count_nodes_below(self, only_leaves=False):
-        """ This method overrides the method of the Node class. Returns 1
-        because the leaf is itself a node. """
+        """
+        This method overrides the method of the Node class. Returns 1
+        because the leaf is itself a node.
+        Args:
+            only_leaves (bool): a boolean indicating if we should count only
+                the leaves.
+        """
         return 1
 
     def __str__(self):
@@ -122,7 +154,15 @@ class Decision_Tree():
     """ This class is the main implementation of the decision tree. """
     def __init__(self, max_depth=10, min_pop=1, seed=0,
                  split_criterion="random", root=None):
-        """ This is the __init__ method. """
+        """
+        This is the __init__ method.
+        Args:
+            max_depth (int): the maximum depth of the tree.
+            min_pop (int): the minimum population of a node to split.
+            seed (int): the seed for the random number generator.
+            split_criterion (str): the criterion to split the tree.
+            root (Node): the root node of the tree.
+        """
         self.rng = np.random.default_rng(seed)
         if root:
             self.root = root
@@ -140,8 +180,13 @@ class Decision_Tree():
         return self.root.max_depth_below()
 
     def count_nodes(self, only_leaves=False):
-        """ This method returns the number of nodes in the tree. If only_leaves
-        is True, count only leaves. """
+        """
+        This method returns the number of nodes in the tree. If only_leaves
+        is True, count only leaves.
+        Args:
+            only_leaves (bool): a boolean indicating if we should count only
+                the leaves.
+        """
         return self.root.count_nodes_below(only_leaves=only_leaves)
 
     def __str__(self):
