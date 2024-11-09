8b856d1e99304ae89ff10eb837b5bff6b98f724c
Alberto Riffaud <alriffaud@gmail.com>
2024-11-09 18:22:43 -0300

    Modification of 0-convolve_grayscale_valid.py file documentation.

--- math/convolutions_and_pooling/0-convolve_grayscale_valid.py
+++ math/convolutions_and_pooling/0-convolve_grayscale_valid.py
@@ -8,11 +8,11 @@ def convolve_grayscale_valid(images, kernel):
     This function performs a valid convolution on grayscale images.
     Args:
         images (np.ndarray): a matrix of shape (m, h, w) containing multiple
-        grayscale images. m is the number of images, h is the height in pixels
-        and w is the width in pixels.
+            grayscale images. m is the number of images, h is the height in
+            pixels and w is the width in pixels.
         kernel (np.ndarray): a matrix of shape (kh, kw) containing the kernel
-        for the convolution. kh is the height of the kernel and kw is the width
-        of the kernel.
+            for the convolution. kh is the height of the kernel and kw is the
+            width of the kernel.
     Returns: a np.ndarray containing the convolved images.
     """
     m, h, w = images.shape
