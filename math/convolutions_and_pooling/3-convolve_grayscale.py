803ea0e0f954df518ef29b60256ec157dae53e9b
Alberto Riffaud <alriffaud@gmail.com>
2024-11-09 18:29:45 -0300

    Modification of 3-convolve_grayscale.py file documentation.

--- math/convolutions_and_pooling/3-convolve_grayscale.py
+++ math/convolutions_and_pooling/3-convolve_grayscale.py
@@ -8,18 +8,18 @@ def convolve_grayscale(images, kernel, padding='same', stride=(1, 1)):
     This function performs convolution on grayscale images with custom padding.
     Args:
         images (np.ndarray): a matrix of shape (m, h, w) containing multiple
-        grayscale images. m is the number of images, h is the height in pixels
-        and w is the width in pixels.
+            grayscale images. m is the number of images, h is the height in
+            pixels and w is the width in pixels.
         kernel (np.ndarray): a matrix of shape (kh, kw) containing the kernel
-        for the convolution. kh is the height of the kernel and kw is the width
-        of the kernel.
+            for the convolution. kh is the height of the kernel and kw is the
+            width of the kernel.
         padding (tuple): a tuple of (Ph, Pw) containing the padding to apply.
-        Ph is the padding for the height and Pw is the padding for the width.
-        If padding is 'same', the output size is the same as the input size.
-        If padding is 'valid', the output size is the input size minus the
-        kernel size plus one.
+            Ph is the padding for the height and Pw is the padding for the
+            width. If padding is 'same', the output size is the same as the
+            input size. If padding is 'valid', the output size is the input
+            size minus the kernel size plus one.
         stride (tuple): a tuple of (Sh, Sw) containing the stride to apply. Sh
-        is the stride for the height and Sw is the stride for the width.
+            is the stride for the height and Sw is the stride for the width.
     Returns: a np.ndarray containing the convolved images.
     """
     m, h, w = images.shape
