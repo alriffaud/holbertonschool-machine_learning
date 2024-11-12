15405edb0ede53380f217c0f9541a5c916400ba1
alriffaud <alriffaud@gmail.com>
2024-11-12 11:17:13 -0300

    Modification of 1-correlation.py file.

--- math/multivariate_prob/1-correlation.py
+++ math/multivariate_prob/1-correlation.py
@@ -13,9 +13,9 @@ def correlation(C):
         An array of shape (d, d) containing the correlation matrix of the data
             set.
     """
-    if not isinstance(C, np.ndarray) or len(C.shape) != 2:
+    if not isinstance(C, np.ndarray):
         raise TypeError("C must be a numpy.ndarray")
-    if C.shape[0] != C.shape[1]:
+    if len(C.shape) != 2 or C.shape[0] != C.shape[1]:
         raise ValueError("C must be a 2D square matrix")
     D = np.diag(1 / np.sqrt(np.diag(C)))
     correlation = np.dot(np.dot(D, C), D)
