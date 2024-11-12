90034c4e33aba68debc839fcf6aabdbd58710481
alriffaud <alriffaud@gmail.com>
2024-11-12 14:21:17 -0300

    Modification of multinormal.py file.

--- math/multivariate_prob/multinormal.py
+++ math/multivariate_prob/multinormal.py
@@ -5,7 +5,6 @@ import numpy as np
 
 class MultiNormal:
     """ This class represents a Multivariate Normal distribution. """
-
     def __init__(self, data):
         """
         This method initializes the MultiNormal object.
@@ -24,3 +23,31 @@ class MultiNormal:
         n = data.shape[1]
         self.mean = np.mean(data, axis=1, keepdims=True)
         self.cov = np.dot(data - self.mean, (data - self.mean).T) / (n - 1)
+
+    def pdf(self, x):
+        """
+        This function calculates the PDF at a data point.
+        Args:
+            x (numpy.ndarray): Is an array of shape (d, 1) containing the
+                data point whose PDF should be calculated. d is the number
+                of dimensions.
+        Returns:
+            The value of the PDF.
+        """
+        if type(x) is not np.ndarray:
+            raise TypeError("x must be a numpy.ndarray")
+        d = self.mean.shape[0]
+        if x.shape != (d, 1):
+            raise ValueError(f"x must have the shape ({d}, 1)")
+        # Calculate the difference between x and the mean
+        diff = x - self.mean
+        # Calculate the determinant and inverse of the covariance matrix
+        cov_det = np.linalg.det(self.cov)
+        cov_inv = np.linalg.inv(self.cov)
+        # Calculate the denominator
+        denom = np.sqrt(((2 * np.pi) ** d) * cov_det)
+        # Calculate the exponent
+        exponent = -0.5 * (diff.T @ cov_inv @ diff)
+        # Calculate the PDF value
+        pdf_value = (1 / denom) * np.exp(exponent)
+        return pdf_value.item()
