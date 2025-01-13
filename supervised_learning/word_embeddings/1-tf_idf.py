fc18027464eae977fd816602608655a9c719c414
Alberto Riffaud <alriffaud@gmail.com>
2025-01-13 13:02:29 -0300

    Modification of 1-tf_idf.py file.

--- supervised_learning/word_embeddings/1-tf_idf.py
+++ supervised_learning/word_embeddings/1-tf_idf.py
@@ -16,23 +16,24 @@ def tf_idf(sentences, vocab=None):
             - embeddings (numpy.ndarray): The TF-IDF matrix of shape (s, f).
             - features (list of str): The vocabulary words used as features.
     """
-    # Preprocess sentences
-    prep_sentences = [re.sub(r"\b(\w+)'s\b", r"\1", sentence.lower())
-                      for sentence in sentences]
+    # Preprocess sentences: lowercase and remove special characters
+    def preprocess(sentence):
+        return re.sub(r'[^a-z\s]', '', sentence.lower())
+
+    processed_sentences = [preprocess(sentence) for sentence in sentences]
 
     # Build the vocabulary if not provided
     if vocab is None:
-        words = []
-        for sentence in prep_sentences:
-            words = re.findall(r'\w+', sentence)
-            words.extend(words)
-        vocab = sorted(set(words))
+        words = set()
+        for sentence in processed_sentences:
+            words.update(sentence.split())
+        vocab = sorted(words)
 
     # Compute TF-IDF for each sentence and term
     tf_idf_vect = TfidfVectorizer(vocabulary=vocab)
 
     # Fit and transform the sentences to produce the TF-IDF matrix
-    tfidf_matrix = tf_idf_vect.fit_transform(sentences)
+    tfidf_matrix = tf_idf_vect.fit_transform(processed_sentences)
 
     # Extract the features (vocabulary used)
     features = tf_idf_vect.get_feature_names_out()
