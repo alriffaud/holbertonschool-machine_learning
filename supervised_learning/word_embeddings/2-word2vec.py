918f0e362349393fc73a700aef0758a335a2c355
alriffaud <alriffaud@gmail.com>
2025-01-14 11:22:18 -0300

    Modification of 2-word2vec.py file.

--- supervised_learning/word_embeddings/2-word2vec.py
+++ supervised_learning/word_embeddings/2-word2vec.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """ This module defines the word2vec_model function. """
-from gensim.models import Word2Vec
+import gensim
 
 
 def word2vec_model(sentences, vector_size=100, min_count=5, window=5,
@@ -27,7 +27,7 @@ def word2vec_model(sentences, vector_size=100, min_count=5, window=5,
     sg = 0 if cbow else 1
 
     # Initialize the Word2Vec model
-    model = Word2Vec(
+    model = gensim.models.Word2Vec(
         sentences=sentences,
         vector_size=vector_size,
         window=window,
