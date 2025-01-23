93ea9c3ce4ca462025830b6ce42d5cdda713483c
alriffaud <alriffaud@gmail.com>
2025-01-23 11:29:24 -0300

    Creation of 4-positional_encoding.py file.

---
+++ supervised_learning/attention/4-positional_encoding.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+""" This module defines the positional_encoding function for a transformer. """
+import numpy as np
+
+
+def positional_encoding(max_seq_len, dm):
+    """
+    This function calculates the positional encoding for a transformer.
+    Args:
+        max_seq_len (int): represents the maximum sequence length.
+        dm (int): is the model depth.
+    Returns:
+        np.ndarray: Array of shape (max_seq_len, dm) containing the positional
+            encoding vectors.
+    """
+    # Initialize the positional encoding matrix
+    PE = np.zeros((max_seq_len, dm))
+
+    # Compute the positional encoding
+    for i in range(max_seq_len):
+        for j in range(0, dm, 2):
+            PE[i, j] = np.sin(i / 10000 ** (j / dm))
+            PE[i, j + 1] = np.cos(i / 10000 ** (j / dm))
+
+    return PE
