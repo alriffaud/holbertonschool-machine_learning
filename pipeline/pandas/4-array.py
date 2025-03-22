0c4481c573dbce3662948c30cf4afdaf76b02c19
Alberto Riffaud <alriffaud@gmail.com>
2025-03-22 12:19:36 -0300

    Creation of 4-array.py file.

---
+++ pipeline/pandas/4-array.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""
+This script defines the function array
+"""
+import numpy as np
+
+
+def array(df):
+    """
+    This function takes a pd.DataFrame as input and performs the following
+    operations:
+    - The function should select the last 10 rows of the High and Close columns
+    - Convert these selected values into a numpy.ndarray
+    Args:
+        df (pd.DataFrame): The input DataFrame to select the last 10 rows
+    Returns:
+        The newly created numpy.ndarray
+    """
+    # Select only the 'Datetime' and 'Close' columns
+    df = df[['High', 'Close']]
+
+    # Select the last 10 rows
+    df = df.tail(10)
+
+    # Convert the DataFrame to a numpy array
+    return np.array(df)
