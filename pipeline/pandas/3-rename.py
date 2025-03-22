daec5eeaea246351f04c7440ed46bbb9a2e6dd36
Alberto Riffaud <alriffaud@gmail.com>
2025-03-22 12:09:36 -0300

    Modification of 3-rename.py file.

--- pipeline/pandas/3-rename.py
+++ pipeline/pandas/3-rename.py
@@ -7,7 +7,8 @@ import pandas as pd
 
 def rename(df):
     """
-    takes a pd.DataFrame as input and performs the following operations:
+    This function takes a pd.DataFrame as input and performs the following
+    operations:
     - The function should rename the Timestamp column to Datetime.
     - Convert the timestamp values to datatime values
     - Display only the Datetime and Close column
