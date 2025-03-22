75605c801257ef1932a61792019e31c57a58be3d
Alberto Riffaud <alriffaud@gmail.com>
2025-03-22 13:19:26 -0300

    Creation of 6-flip_switch.py file.

---
+++ pipeline/pandas/6-flip_switch.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""
+This script defines the function flip_switch
+"""
+import pandas as pd
+
+
+def flip_switch(df):
+    """
+    This function takes a pd.DataFrame as input and performs the following
+    operations:
+    - Sorts the data in reverse chronological order.
+    - Transposes the sorted dataframe.
+    Args:
+        df (pd.DataFrame): The input DataFrame
+    Returns:
+        The transformed DataFrame
+    """
+    # Sort the DataFrame by the 'Timestamp' column in descending order
+    df_sorted = df.sort_values(by='Timestamp', ascending=False)
+
+    # Transpose the sorted DataFrame
+    df_transpose = df_sorted.T
+
+    return df_transpose
