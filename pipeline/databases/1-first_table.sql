209372a6fb7772001a5c8fad74cba856e359f03c
Alberto Riffaud <alriffaud@gmail.com>
2025-03-24 15:00:06 -0300

    Creation of 1-first_table.sql file.

---
+++ pipeline/databases/1-first_table.sql
@@ -0,0 +1,7 @@
+-- Write a script that creates a table called first_table in the current
+-- database in a MySQL server.
+
+CREATE TABLE IF NOT EXISTS first_table (
+    id INT,
+    name VARCHAR(256)
+    );
