e0a3c494e1610f42729bc2dbc3aee0275e15d746
Alberto Riffaud <alriffaud@gmail.com>
2025-03-29 16:52:16 -0300

    Modification of 34-log_stats.py file.

--- pipeline/databases/34-log_stats.py
+++ pipeline/databases/34-log_stats.py
@@ -6,27 +6,27 @@ from pymongo import MongoClient
 METHODS = ["GET", "POST", "PUT", "PATCH", "DELETE"]
 
 
-def get_logs_stats(mongo_collection):
+def get_logs_stats(mongo_collection, option=None):
     """
     This function gets statistics about Nginx logs stored in MongoDB.
     Args:
         mongo_collection: the pymongo collection object.
         option: the method to search for.
     """
-    total_logs = mongo_collection.count_documents({})
-    print(f"{total_logs} logs")
+    items = {}
+    if option:
+        value = mongo_collection.count_documents(
+            {"method": {"$regex": option}})
+        print("\tmethod {}: {}".format(option, value))
+        return
 
-    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    result = mongo_collection.count_documents(items)
+    print("{} logs".format(result))
     print("Methods:")
-    for method in methods:
-        count = mongo_collection.count_documents({"method": method})
-        print(f"\tmethod {method}: {count}")
-
-    status_check_count = mongo_collection.count_documents({
-        "method": "GET",
-        "path": "/status"
-    })
-    print(f"{status_check_count} status check")
+    for method in METHODS:
+        get_logs_stats(nginx_collection, method)
+    status_check = mongo_collection.count_documents({"path": "/status"})
+    print("{} status check".format(status_check))
 
 
 if __name__ == "__main__":
