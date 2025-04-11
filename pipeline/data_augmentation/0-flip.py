51d0c5928e5ea696f26dbfd8ff62f202538f5460
alriffaud <alriffaud@gmail.com>
2025-04-11 10:35:00 -0300

    Modification of 0-flip.py file.

--- pipeline/data_augmentation/0-flip.py
+++ pipeline/data_augmentation/0-flip.py
@@ -11,7 +11,7 @@ def flip_image(image):
     Returns:
     tf.Tensor: The horizontally flipped image.
     """
-    # Use TensorFlow's image flipping function to reverse the image along
+    # TensorFlow's image flipping function to reverse the image along
     # the width axis
     flipped = tf.image.flip_left_right(image)
     return flipped
